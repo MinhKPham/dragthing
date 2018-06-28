13d36246a16a896f51792f66d5d1b3888620f0ba
MinhKPham <phamm@rpi.edu>
2018-06-28 15:57:53 -0400

    first commit

---
+++ Assets/drag.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+public class drag : MonoBehaviour {
+    public float d = 0f;
+    public bool rotate=false;
+    public bool theone = true;
+    public float rotspeed = 3f;
+    public Text ro;
+    public Text po;
+    float rotx = 0;
+    float roty = 0;
+    float rotz = 0;
+    Vector3 lastpo;
+    Quaternion lastro;
+
+    Quaternion fromrot;
+    Quaternion torot;
+    // Use this for initialization
+    void Start () {
+        lastpo = transform.position;
+        lastro = transform.rotation;
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!theone)
+        {
+            lastpo = transform.position;
+            lastro = transform.rotation;
+        }
+        if (theone)
+        {
+            ro.text = lastro.ToString();
+            po.text = lastpo.ToString();
+        }
+        
+
+    }
+    public void Reset()
+    {
+        transform.position=lastpo;
+        transform.rotation=lastro;
+    }
+    void increasez()
+    {
+        rotz += 1;
+    }
+    void dereasez()
+    {
+        rotz -= 1;
+    }
+
+    private void OnMouseDrag()
+    {
+        
+        //actionlog.Close();
+        if (!theone) return;
+        Vector3 mousePosition = Input.mousePosition;
+
+        mousePosition.z = (Vector3.Project(Camera.main.transform.position- transform.position, Camera.main.transform.forward)).magnitude;
+
+        Vector3 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        
+        if (rotate)
+        {
+            rotx = Input.GetAxis("Mouse X") * rotspeed * Mathf.Deg2Rad;
+            roty = Input.GetAxis("Mouse Y") * rotspeed * Mathf.Deg2Rad;
+            //fromrot = transform.rotation;
+            //torot = Quaternion.Euler(roty, rotx, rotz);
+            //transform.rotation = Quaternion.Lerp(fromrot, torot, Time.deltaTime * 100);
+            transform.RotateAround(Camera.main.transform.up, -rotx);
+            transform.RotateAround(Camera.main.transform.right, roty);
+            StreamWriter actionlog = new StreamWriter("Assets/action.txt", true);
+            
+
+
+        }
+        else
+        {
+            
+            
+            transform.position = objPosition;
+            print(objPosition);
+        }
+
+    }
+    
+}
---
+++ Assets/master.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System.Text;
+public class master : MonoBehaviour {
+    drag d;
+    StreamWriter actionlog;
+    // Use this for initialization
+    Object[] allthings;
+    float rotx = 0;
+    float roty = 0;
+    Quaternion fromrot;
+    Quaternion torot;
+    public GameObject cam;
+    int i = 0;
+    public Text b;
+    public Text objname;
+    bool r = false;
+	void Start () {
+        actionlog = new StreamWriter("Assets/action.txt",true);
+        actionlog.WriteLine("start");
+        //actionlog.Close();
+        allthings = Resources.FindObjectsOfTypeAll(typeof(drag));
+        print("dsdsd"+allthings.Length);
+        
+    }
+    public void prev()
+    {
+        i -= 1;
+        actionlog.WriteLine("move list backward");
+    }
+    public void  next()
+    {
+        i += 1;
+        actionlog.WriteLine("move list forward");
+    }
+    public void rotate()
+    {
+        r=!r;
+        if (r) { b.text = "rotating"; actionlog.WriteLine("change to rotating"); }
+        else { b.text = "not rotating"; actionlog.WriteLine("change to moving"); }
+    }
+
+    public void center()
+    {
+        
+        cam.transform.LookAt(d.gameObject.transform);
+        actionlog.WriteLine("center");
+    }
+    public void rotatecamleft()
+    {
+
+        cam.transform.RotateAround(d.transform.position, cam.transform.up, 1);
+        actionlog.WriteLine("rotate cam left");
+    }
+    public void rotatecamright()
+    {
+        cam.transform.RotateAround(d.transform.position, cam.transform.up, -1);
+        actionlog.WriteLine("rotate cam right");
+
+    }
+    public void rotatecamup()
+    {
+        cam.transform.RotateAround(d.transform.position, cam.transform.right, 1);
+        actionlog.WriteLine("rotate cam up");
+
+    }
+    public void rotatecamdown()
+    {
+        cam.transform.RotateAround(d.transform.position, cam.transform.right, -1);
+        actionlog.WriteLine("rotate cam down");
+
+    }
+    public void camin()
+    {
+        cam.transform.position += cam.transform.forward * 1;
+        actionlog.WriteLine("zoom in");
+
+    }
+    public void camout()
+    {
+        cam.transform.position += cam.transform.forward * -1;
+        actionlog.WriteLine("zoom out");
+
+
+    }
+    public void backto()
+    {
+        d.Reset();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (i >= allthings.Length) i = 0;
+        else if (i < 0) i = allthings.Length - 1;
+        
+        for (int x = 0; x < allthings.Length; x++)
+        {
+            drag temd = (drag)allthings[x];
+            temd.theone = false;
+        }
+        d= (drag)allthings[i];
+        d.theone = true;
+        objname.text = d.gameObject.name;
+        d.rotate = r;
+        
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            camin();
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            camout();
+        }
+        if (Input.GetKey(KeyCode.V))
+        {
+            backto();
+        }
+
+
+    }
+    private void OnDestroy()
+    {
+        actionlog.Close();
+    }
+}
